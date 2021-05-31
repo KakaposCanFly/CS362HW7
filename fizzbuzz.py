1245986ab73f5bbbe80cb5333b07d44dc337776c
Cole Theodore <theodoco@oregonstate.edu>
2021-05-31 15:55:26 -0700

    Implemented test1 method -- passes test1 if fizz_buzz(a) takes a number that is divisible by 3

---
+++ fizzbuzz.py
@@ -0,0 +1,4 @@
+def fizz_buzz(a):
+    if a % 3 == 0:
+        x = "Fizz"
+    return x
--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -1,4 +1,5 @@
 import unittest
+import fizzbuzz
 
 class Testing(unittest.TestCase):
     def test_test1(self):
