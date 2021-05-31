cc831a79110ebbe33e84ad253560c0a11f9307ab
Cole Theodore <theodoco@oregonstate.edu>
2021-05-31 15:57:55 -0700

    Wrote second test -- checks if the values 5, 10, 20, and 25 return 'Buzz'

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -6,5 +6,9 @@ class Testing(unittest.TestCase):
         for i in [3, 6, 12, 21]:
             self.assertEqual(fizzbuzz.fizz_buzz(i), "Fizz")
 
+    def test_test2(self):
+        for i in [5, 10, 20, 25]:
+            self.assertEqual(fizzbuzz.fizz_buzz(i), "Buzz")
+
 if __name__ == "__main__":
     unittest.main()
