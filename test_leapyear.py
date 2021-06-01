ce790686bfde1850de20a98c1aaa3272f1cb43b4
Cole Theodore <theodoco@oregonstate.edu>
2021-05-31 17:57:36 -0700

    Wrote third test -- the input of 2000 is evenly divisible by 4, 100, and 400

--- test_leapyear.py
+++ test_leapyear.py
@@ -8,6 +8,9 @@ class Testing(unittest.TestCase):
     def test_leapYear2(self):
         self.assertFalse(leapyear.is_leap_year(2100))
 
+    def test_leapYear3(self):
+        self.assertTrue(leapyear.is_leap_year(2000))
+
 
 if __name__ == "__main__":
     unittest.main()
